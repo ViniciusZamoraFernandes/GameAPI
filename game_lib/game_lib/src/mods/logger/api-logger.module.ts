6aa96c59f6b993af4cd4a061e0e1a9f1c5863a44
ViniciusZamoraFernandes <viniciuszamoragithub@gmail.com>
2026-05-08 22:42:51 -0300

    Delete game_lib/game_lib directory

--- game_lib/game_lib/src/mods/logger/api-logger.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ApiLoggerService } from './api-logger.service';
-
-@Module({ providers: [ApiLoggerService], exports: [ApiLoggerService] })
-export class ApiLoggerModule {}
