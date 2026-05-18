02c6eb7d31db36aa8dd5319257b6a4342aea3d48
ViniciusZamoraFernandes <viniciuszamoragithub@gmail.com>
2026-05-18 15:03:42 -0300

    Enhance bootstrap with environment variable checks
    Added error handling for missing WEB_URL environment variable and updated CORS settings.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { InternalServerErrorException } from "@nestjs/common";
+
+const noEnvException = (env_name: string) =>
+  new InternalServerErrorException(
+    No '${env_name}' environment variable was found!,
+  );
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+  const port = process.env.PORT ?? 3000;
+  const web_url = process.env.WEB_URL;
+  if (!web_url) throw noEnvException("WEB_URL");
+
+  app.enableCors({ origin: [app_url, web_url], credentials: true });
+  await app.listen(port);
 }
 bootstrap();
